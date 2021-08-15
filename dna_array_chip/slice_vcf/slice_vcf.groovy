9b931ea741c67e4d845332b4a017a30da1b47fdb
hothman <houcemoo@gmail.com>
2021-08-15 21:35:01 +0100

    identifying matcing chr is now groovy controlled

--- dna_array_chip/slice_vcf/slice_vcf.groovy
+++ dna_array_chip/slice_vcf/slice_vcf.groovy
@@ -1,19 +1,24 @@
 #!/usr/bin/env nextflow
 
 // path to bed file containing list of genes to extract
-params.BED="all_ADME.bed"
+params.BED="../../../data/all_ADME.bed"
 
 VCF = Channel.fromPath( '../simulate_vcf/*.vcf.gz' )
-params.INDEXHOME= 'simulate_vcf' 
+params.INDEXHOME= '/home/houcem/tmp_science/coverage_chip/NGS/dna_array_chip/simulate_vcf' 
 genes = Channel.fromPath("$params.BED").splitText()  { it.replaceAll("\n", "") }
 
-	//#bcftools view -r $chr:$start-$end $path_to_vcf -Oz -o $outputfile 
-    //#tabix  $outputfile
+		/* In this bloc, you need to specify the association between the files and the 
+		 Chromosome Ids
+		*/
+vcf_map_chromosome = [ "dummy_chr22.vcf.gz":"22",
+						"dummy_chr21.vcf.gz":"21",]
+
+
 
 params.upstream_buffer = 1000000
 params.downstream_buffer = 1000000
 
-process getbed {
+process GetBed {
 	input: 
 		val gene from genes.flatMap() 
 	output: 
@@ -23,40 +28,46 @@ process getbed {
 	"""
 	echo $gene > tmp.bed
 	gene_name=\$(awk {'print \$4'}  tmp.bed)
-	bed_info.py  --bed tmp.bed --upstream ${params.upstream_buffer} --downstream  ${params.downstream_buffer} --outputbed \${gene_name}_adme.bed
+	chr=\$(awk {'print \$1'}  tmp.bed)
+	bed_info.py  --bed tmp.bed --upstream ${params.upstream_buffer} --downstream  ${params.downstream_buffer} --outputbed \${gene_name}_chr\${chr}_adme.bed
 	"""
 }
 
 
-process sliceVCF {
-	conda 'bioconda::bcftools'
-	errorStrategy 'ignore'
+// WARNING: ethe process ignores errors assuming that it is due 
+// to unm,atching chromosome ID in deb and vcf file 
+
+process SliceVCF {
+	//conda 'bioconda::bcftools'
 	publishDir './sliced_vcfs' , mode: 'copy', overwrite: true
+	//echo true
+	errorStrategy 'ignore'
+
 	input: 
-		file bed from bed_adme
 		each file(vcf) from VCF
+		file(bed) from bed_adme
 
 	output: 
 		file("*_sliced.vcf.gz") optional true into sliced_vcfs
 
-	"""
-	gene_name=\$(awk {'print \$4'} $bed)
-	chromosome_id=\$(awk {'print \$1'} $bed)
-	chromosome_in_vcf=\$(bcftools query -f '%CHROM\n' $vcf | uniq)
-	ln -s ${params.INDEXHOME}/${vcf}.tbi
-	if [ \$chromosome_id -eq \$chromosome_in_vcf ]
-	then
-		bcftools view  -R $bed -O z $vcf -o  \${gene_name}_chr\${chromosome_id}_sliced.vcf.gz
-		
-	fi
-	
-	"""
+	script:
+		chrom_id_from_map = vcf_map_chromosome[vcf.name]
+
+		def m = bed.name =~ /chr[0-9]{1,2}/
+		matching_string = m.find()?m.group():"not matched"
+		chrom_id = matching_string.replaceAll("chr", "")
+
+		if (chrom_id == chrom_id_from_map)
+			"""
+			gene_name=\$(awk {'print \$4'} $bed)
+			ln -s ${params.INDEXHOME}/${vcf}.tbi
+			bcftools view  -R $bed -O z $vcf -o \${gene_name}_chr${chrom_id}_sliced.vcf.gz
+			"""
 }
 
 
-
-process compressIndex {
-	conda 'bioconda::bcftools'
+process IndexVCF {
+	//conda 'bioconda::bcftools'
 	publishDir './sliced_vcfs' , mode: 'move', overwrite: true
 
 	input:
@@ -66,7 +77,7 @@ process compressIndex {
 
 	"""
 	bcftools index $vcf -t 
-
 	"""
 }
+
 
