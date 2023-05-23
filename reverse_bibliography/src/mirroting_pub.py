cb169af44fad545f1b2059d1cf27014a5be318bf
houcem <houcemoo@gmail.com>
2023-05-23 06:43:29 +0100

    generate and output list of replicated pmid dates

--- reverse_bibliography/src/mirroting_pub.py
+++ reverse_bibliography/src/mirroting_pub.py
@@ -1,4 +1,6 @@
 from metapub import PubMedFetcher
+from tqdm import tqdm
+from random import randint
 
 
 class readList(object):
@@ -9,9 +11,48 @@ class readList(object):
         with open(self.list_pmid) as input:
             self.pmids = [line.strip() for line in input.readlines()]
 
+    def getYears(self):
+        self.years = []
+        fetch = PubMedFetcher()
+        for pmid in tqdm(self.pmids):
+            article = fetch.article_by_pmid(pmid)
+            self.years.append(article.year)
+        del fetch
+        return self.years
+
+
+class mirroringPubmed(object):
+    def __init__(self, years):
+        self.years = years
+
+    def _sampleFromList(self, list_of_articles):
+        ran = randint(0, 1000)
+        return list_of_articles[ran]
+
+    def fetcher(self, negative_pmids, n_samples=2):
+        replicated_pmids = []
+        fetch = PubMedFetcher()
+        for year in tqdm(self.years):
+            query = year+"[DP] NOT 'Pharmacogenetics'[Mesh] NOT review[PT]"
+            my_hits = fetch.pmids_for_query(query, retmax=1000)
+            i = 1
+            while i <= n_samples:
+                selected = self._sampleFromList(my_hits)
+                while selected in negative_pmids:
+                    selected = self._sampleFromList(my_hits)
+                replicated_pmids.append(selected)
+                i += 1
+        return replicated_pmids
+
 
 if __name__ == "__main__":
     list = readList("../data/pmids.txt")
     list.readPmids()
-    pmid_list = list.pmids
-    print(pmid_list)
+    years_to_replicate = list.getYears()
+    pmids_to_replicate = list.pmids
+    replicate_years = mirroringPubmed(years_to_replicate)
+    replicated_pmids = replicate_years.fetcher(
+        negative_pmids=pmids_to_replicate)
+    with open("negative_pmids.txt", "w") as output:
+        for pmid in replicated_pmids:
+            output.write(pmid+"\n")
